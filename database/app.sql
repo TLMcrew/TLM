bc3f8653233fca8d1e043a47d74bc28a7d87c6ad
Stephen Johnson <stephendjohnson420@gmail.com>
2022-03-29 05:39:43 -0500

    Updated Graphs Branch
    Got the graphs branch updated with main

---
+++ database/app.sql
@@ -0,0 +1,126 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 22, 2022 at 07:59 PM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 7.4.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `app`
+--
+CREATE DATABASE IF NOT EXISTS `app` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `app`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `userdata`
+--
+
+DROP TABLE IF EXISTS `userdata`;
+CREATE TABLE `userdata` (
+  `data_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `height` int(11) NOT NULL,
+  `weight` int(11) NOT NULL,
+  `calories` int(11) NOT NULL,
+  `sleep` int(11) NOT NULL,
+  `hydration` int(11) NOT NULL,
+  `exercise` int(11) NOT NULL,
+  `date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `userdata`
+--
+
+INSERT INTO `userdata` (`data_id`, `user_id`, `height`, `weight`, `calories`, `sleep`, `hydration`, `exercise`, `date`) VALUES
+(1, 1, 70, 190, 490, 7, 4, 30, '2022-03-22'),
+(2, 2, 65, 150, 321, 4, 1, 1, '2022-03-22'),
+(3, 3, 65, 170, 90, 15, 3, 15, '2022-03-22'),
+(4, 1, 0, 0, 455, 0, 0, 0, '2022-03-22'),
+(5, 3, 0, 0, 233, 0, 0, 15, '2022-03-22');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
+  `firstname` varchar(255) NOT NULL,
+  `lastname` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `dob` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `firstname`, `lastname`, `email`, `dob`) VALUES
+(1, 'Logan', 'Grisham', 'lgrisham@my.apsu.edu', '1997-04-30'),
+(2, 'Sebastian', 'Hardesty', 'shardesty@my.apsu.edu', '2000-06-05'),
+(3, 'Stephen', 'Johnson', 'sjohnson155@my.apsu.edu', '2000-08-11');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `userdata`
+--
+ALTER TABLE `userdata`
+  ADD PRIMARY KEY (`data_id`),
+  ADD KEY `user_userdata_idx` (`user_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `userdata`
+--
+ALTER TABLE `userdata`
+  MODIFY `data_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `userdata`
+--
+ALTER TABLE `userdata`
+  ADD CONSTRAINT `user_userdata_fk` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ public_html/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title> Total Life manager </title>
+</head>
+
+<body>
+    <h1>
+        placeholder php
+    </h1>
+    <?php
+    require ("../systemData.php");
+    
+    $connection = mysqli_connect($DB_SERVER,$DB_USER,$DB_PASSWORD,$DB_NAME);
+
+    if (!$connection) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+
+    $sql = "SELECT * FROM users;";
+    $result = mysqli_query($connection, $sql);
+    #$resultchecker = mysql_num_rows($result);
+    $resultchecker = 1;
+
+    if($resultchecker > 0){
+        while($row = mysqli_fetch_assoc($result)){
+            echo $row['user_id'];
+        }
+    }
+
+    
+    else {
+        echo "no data";
+    }
+    $connection->close();
+    
+    ?>
+
+</body>
+
+</html>
