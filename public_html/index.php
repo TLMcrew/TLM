12e558b079e76237b0215de18218886dbf9ab9ff
Logan Grisham <71851052+LoganGrisham@users.noreply.github.com>
2022-04-01 10:16:26 -0500

    changed routing

--- public_html/index.php
+++ public_html/index.php
@@ -4,7 +4,7 @@ require ("../systemData.php");
 session_start();
 
 if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
-    header("location: login.php");
+    header("location: register.php");
     exit;
 }
 ?>
