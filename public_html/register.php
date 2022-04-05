f770e268765705c528d0b0a87fc4e230e41ae204
Logan Grisham <71851052+LoganGrisham@users.noreply.github.com>
2022-04-05 13:48:48 -0500

    more output testing

--- public_html/register.php
+++ public_html/register.php
@@ -83,7 +83,8 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                 header("location: login.php");
             } else{
                 echo "Oops! Something went wrong. Please try again later.";
-                echo $username;
+                echo "($username)";
+                echo "username";
             }
 
             // Close statement
