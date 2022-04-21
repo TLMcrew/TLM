eba61622a06f10d19dbcb72645354730da93acd7
Logan Grisham <71851052+LoganGrisham@users.noreply.github.com>
2022-04-21 13:18:07 -0500

    reformatted login.php

--- public_html/login.php
+++ public_html/login.php
@@ -109,10 +109,9 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
       </nav>
     </header>
     <script src="../js/hamburger.js"></script>
-    <div class="wrapper">
-        <h2>Login</h2>
-        <p>Please fill in your credentials to login.</p>
-
+    <div class="loginRegister">
+      <div class="login">
+        <h1>Login</h1>
         <?php 
         if(!empty($login_err)){
             echo '<div class="alert alert-danger">' . $login_err . '</div>';
@@ -120,21 +119,56 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         ?>
 
         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-            <div class="form-group">
+            <div class="txt_field">
                 <label>Username</label>
                 <input type="text" name="username" class="form-control <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $username; ?>">
                 <span class="invalid-feedback"><?php echo $username_err; ?></span>
             </div>    
-            <div class="form-group">
+            <div class="txt_field">
                 <label>Password</label>
                 <input type="password" name="password" class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>">
                 <span class="invalid-feedback"><?php echo $password_err; ?></span>
             </div>
-            <div class="form-group">
+            <div class="pass">Forgot Password?</div>
                 <input type="submit" class="btn btn-primary" value="Login">
             </div>
             <p>Don't have an account? <a href="register.php">Sign up now</a>.</p>
         </form>
     </div>
+    <div class="register">
+        <h1>Register</h1>
+        <form method="post">
+          <div class = "username">
+          <div class="txt_field">
+            <input type="text" required />
+            <span></span>
+            <label>Username</label>
+          </div>
+          <div class="txt_field">
+            <input type="text" required />
+            <span></span>
+            <label>Email</label>
+          </div>
+        </div>
+
+        <div class = "password">
+          <form method="post">
+            <div class = "username">
+            <div class="txt_field">
+              <input type="password" name="password" required />
+              <span></span>
+              <label>Password</label>
+            </div>
+            <div class="txt_field">
+              <input type="password" name="confirmPassword" required />
+              <span></span>
+              <label>Confirm Password</label>
+            </div>
+        </div>
+          <input type="submit" value="Register" />
+        </form>
+      </div>
+    </div>
+
 </body>
 </html>
