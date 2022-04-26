305e5a445cc9e0600c03529e85a3b60af2328164
Logan Grisham <71851052+LoganGrisham@users.noreply.github.com>
2022-04-26 14:01:49 -0500

    refactored all html to php

--- public_html/index.php
+++ public_html/index.php
@@ -5,7 +5,7 @@ require ("../systemData.php");
 session_start();
 
 if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
-    header("location: register.php");
+    header("location: login.php");
     exit;
 }
 ?>
